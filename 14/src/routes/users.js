54149296d9d84645ef3659965007787bb1b2f886
Максим <160880502+elow1111@users.noreply.github.com>
2024-11-22 19:21:06 +0300

    users.js

--- 14/src/routes/users.js
+++ 14/src/routes/users.js
@@ -8,6 +8,32 @@ export default (app) => {
   app.get("/users/new", (req, res) => res.view("src/views/users/new"));
 
   // BEGIN (write your solution here)
+  app.post("/users", (req, res) => {
+    const { firstName, lastName, email } = req.body;
 
+    const newUser = {
+      id: generateId(),
+      firstName,
+      lastName,
+      email,
+      token: generateToken(),
+    };
+
+    users.push(newUser);
+
+    res.setCookie('token', newUser.token, { httpOnly: true });
+    res.redirect(`/users/${newUser.id}`);
+  });
+
+  app.get("/users/:id", (req, res) => {
+    const { id } = req.params;
+    const user = users.find((u) => u.id === id);
+
+    if (!user || req.cookies.token !== user.token) {
+      return res.send("User not found");
+    }
+
+    res.view("src/views/users/show", { user });
+  });
   // END
 };
