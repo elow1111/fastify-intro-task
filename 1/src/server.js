1111ee0973ae76e22575c9172c414fe417566799
Максим <160880502+elow1111@users.noreply.github.com>
2024-11-22 19:05:52 +0300

    server.js

--- 1/src/server.js
+++ 1/src/server.js
@@ -3,7 +3,8 @@ import fastify from "fastify";
 export default () => {
   const app = fastify();
   // BEGIN (write your solution here)
-
+  app.get('/', async () => {
+    return 'Welcome to Fastify!';
   // END
   return app;
 };
