d26fe6359426504f040a6beb50872ff401429969
Максим <160880502+elow1111@users.noreply.github.com>
2024-11-22 19:23:05 +0300

    sessions.js

--- 15/src/routes/sessions.js
+++ 15/src/routes/sessions.js
@@ -4,6 +4,26 @@ export default (app) => {
   const users = generateUsers();
 
   // BEGIN (write your solution here)
+  app.get("/sessions/new", (req, res) => {
+    const errorMessage = req.query.error ? "Wrong username or password" : null;
+    res.view("src/views/sessions/new", { errorMessage });
+  });
 
+  app.post("/sessions", (req, res) => {
+    const { username, password } = req.body;
+    const user = users.find(u => u.username === username);
+
+    if (user && decrypt(user.password) === password) {
+      req.session.userId = user.id;
+      return res.redirect("/");
+    }
+
+    res.redirect("/sessions/new?error=true");
+  });
+
+  app.post("/sessions/delete", (req, res) => {
+    delete req.session.userId;
+    res.redirect("/");
+  });
   // END
 };
