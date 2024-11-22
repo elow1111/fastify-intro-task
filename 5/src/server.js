8421289273497a802d8fb94b16c16837e78fa832
Максим <160880502+elow1111@users.noreply.github.com>
2024-11-22 19:16:09 +0300

    server.js

--- 5/src/server.js
+++ 5/src/server.js
@@ -9,7 +9,26 @@ export default async () => {
   const users = getUsers();
 
   // BEGIN (write your solution here)
+  app.register(view, {
+    engine: {
+      pug,
+    },
+  });
 
+  app.get('/users', async (request, reply) => {
+    return reply.view('/src/views/users/index.pug', { users });
+  });
+
+  app.get('/users/:id', async (request, reply) => {
+    const userId = request.params.id;
+    const user = users.find(u => u.id === userId);
+
+    if (!user) {
+      return reply.code(404).send('User not found');
+    }
+
+    return reply.view('/src/views/users/show.pug', { user });
+  });
   // END
 
   return app;
