fcdf0d2ad8262914e413574ccd9ee6e2adf1e3f9
Максим <160880502+elow1111@users.noreply.github.com>
2024-11-22 19:12:13 +0300

    server.js

--- 4/src/server.js
+++ 4/src/server.js
@@ -7,7 +7,17 @@ export default () => {
   const companies = getCompanies();
 
   // BEGIN (write your solution here)
+  app.get('/companies/:id', async (request, reply) => {
+    const { id } = request.params;
 
+    const company = companies.find(company => company.id === id);
+
+    if (company) {
+      return company;
+    } else {
+      reply.status(404).send('Company not found');
+    }
+  });
   // END
 
   return app;
