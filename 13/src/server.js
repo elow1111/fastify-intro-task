4fb22a574deb3f798c3a785b597d64431e22d6da
Максим <160880502+elow1111@users.noreply.github.com>
2025-01-14 13:44:51 +0300

    server.js

--- 13/src/server.js
+++ 13/src/server.js
@@ -10,18 +10,13 @@ export default async () => {
 
   await app.register(view, { engine: { pug } });
   await app.register(formbody);
-
-  // BEGIN (write your solution here)
   await app.register(middie);
 
-  const setCacheControlHeader = (req, res, next) => {
-    if (!res.getHeader('Cache-Control')) {
-      res.setHeader('Cache-Control', 'no-store');
-    }
+  // BEGIN (write your solution here)
+  app.use((req, res, next) => {
+    res.setHeader("Cache-Control", "no-store");
     next();
-  };
-
-  app.use(setCacheControlHeader);
+  })
   // END
 
   app.get("/", (req, res) => res.view("src/views/index"));
