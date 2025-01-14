ad742285b2f83785ac5a1c0514348d246a59ecde
Максим <160880502+elow1111@users.noreply.github.com>
2025-01-14 13:45:18 +0300

    users.js

--- 14/src/routes/users.js
+++ 14/src/routes/users.js
@@ -5,35 +5,31 @@ export default (app) => {
 
   const generateId = buildIdGenerator();
 
-  app.get("/users/new", (req, res) => res.view("src/views/users/new"));
-
-  // BEGIN (write your solution here)
-  app.post("/users", (req, res) => {
-    const { firstName, lastName, email } = req.body;
-
-    const newUser = {
-      id: generateId(),
-      firstName,
-      lastName,
-      email,
-      token: generateToken(),
-    };
-
-    users.push(newUser);
-
-    res.setCookie('token', newUser.token, { httpOnly: true });
-    res.redirect(`/users/${newUser.id}`);
-  });
+  app.get("/users/new", (req, res) => res.render("src/views/users/new"));
 
   app.get("/users/:id", (req, res) => {
     const { id } = req.params;
+    const token = req.cookies.token; 
+
     const user = users.find((u) => u.id === id);
 
-    if (!user || req.cookies.token !== user.token) {
-      return res.send("User not found");
+    if (!user || token !== user.token) {
+      return res.status(404).send("User not found");
     }
 
     res.view("src/views/users/show", { user });
   });
-  // END
+
+  app.post("/users", (req, res) => {
+    const token = generateToken();
+    res.cookie("token", token);
+
+    const { firstName, lastName, email } = req.body;
+    const id = generateId();
+
+    const user = { id, firstName, email, lastName, token };
+    users.push(user);
+
+    res.redirect(`/users/${id}`);
+  });
 };
